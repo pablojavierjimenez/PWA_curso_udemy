8121c154d2accc9d6548544ae60c602accea292b
Pablo Jimenez <pablojavierjimenez@gmail.com>
2020-09-08 16:09:44 -0300

    testing new fetch on js

---
+++ 01_fundamentos/fetch-2.js
@@ -0,0 +1,18 @@
+
+
+
+fetch( "https://reqres.in/api/users")
+    .then( data => data.json())
+    .then( resp => {
+        console.log(resp);
+        console.log(resp.page)
+        console.log(resp.per_page)
+    })
+
+
+fetch("http://google.com")
+    .then( res => res.text() )
+    .then( html => {
+        var container = document.getElementById('container');        
+        container.innerHTML = html;
+    })
