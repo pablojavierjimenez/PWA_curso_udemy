34c030cf4e04537d36e2f0e8a042f2098cee79e2
Pablo Jimenez <pablojavierjimenez@gmail.com>
2020-09-11 16:49:25 -0300

    SW: modificando la respuesta de la peticion fetch

--- 02_serviiceWorkerAndFetchEvent/sw.js
+++ 02_serviiceWorkerAndFetchEvent/sw.js
@@ -1,11 +1,26 @@
 self.addEventListener('fetch', event => {
     // step: 1
-    if (event.request.url.includes('.jpg')) {
-        // let defaultImg = null;
-        let defaultImg = fetch( event.request.url);        
-        event.respondWith(defaultImg)
-    }
+    // if (event.request.url.includes('.jpg')) {
+    //     // let defaultImg = null;
+    //     let defaultImg = fetch( event.request.url);        
+    //     event.respondWith(defaultImg)
+    // }
 
-    
+    //step: 2
+    if ( event.request.url.includes( 'style.css' ) ) {
+
+        let res = new Response(`
+            body {
+                background-color: red !important;
+                color: black;
+            }
+        `,{
+            headers: {
+                'Content-Type': 'text/css'
+            }
+        });
+
+        event.respondWith( res );
+    }
 
 });
