fe2ffe219ceee01536107be49dafa1df68cdd992
Pablo Jimenez <pablojavierjimenez@gmail.com>
2020-09-18 09:10:30 -0300

    SW LifeCycle: Activate, and skipWaiting function

--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -12,4 +12,21 @@ self.addEventListener('install', event => {
      * o la nueva version de el mismo esten siendo instalados
      */
     console.log('sw-v2:', event)
+
+    // Esta funcion fuerza a la activacion de la nueva version del SW
+    // pero no es recomendable utilizarla por riesgo a perder informacion sensible
+    // durante la activacion de la nueva version,
+    // por eso es que lo dejaremos comentado por ahora
+
+    self.skipWaiting();
+})
+
+// Cuando el SW toma el control
+
+self.addEventListener('activate', event => {
+    /**
+     * Normalmente lo que se hace en este listener es:
+     * - remover cache de la version anterior
+     */
+    console.log('SW-v2: Activate event: ', event)
 })
