080971b5dcc6b7646bb89212161f4ecdd82d435d
Pablo Jimenez <pablojavierjimenez@gmail.com>
2020-09-18 09:35:27 -0300

    SW LifeCycle: installation and event.waitUntil

--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -3,6 +3,7 @@
 
 self.addEventListener('install', event => {
     /**
+     * STEP: 1
      * que hacemos normalmente en este hook:
      * - cargamos assets
      * - creariamos cache etc.
@@ -13,18 +14,42 @@ self.addEventListener('install', event => {
      */
     console.log('sw-v2:', event)
 
-    // Esta funcion fuerza a la activacion de la nueva version del SW
-    // pero no es recomendable utilizarla por riesgo a perder informacion sensible
-    // durante la activacion de la nueva version,
-    // por eso es que lo dejaremos comentado por ahora
+    /**
+     * STEP: 2-b
+     * Esta funcion fuerza a la activacion de la nueva version del SW
+     * pero no es recomendable utilizarla por riesgo a perder informacion sensible
+     * durante la activacion de la nueva version,
+     * por eso es que lo dejaremos comentado por ahora
+     */
+    // self.skipWaiting();
 
-    self.skipWaiting();
+    /**
+     * STEP: 3
+     * en esta parte simularemos con un timeout la carga/instalacion
+     * de archivos, para poder probar la funcion waitUntil
+     
+     * STEP: 3-b
+     * para poder escapar de los errores de sincronismo
+     * utilizaremos la funcion  waitUntil en conbinacion
+     * con una promesa
+     */
+    const onInstallationEnd = new Promise( ( resolve, reject ) => {
+        
+        setTimeout( () => {
+            self.skipWaiting();
+            resolve();
+            console.log('SW: La simulacion de descarga finalizo!')
+        }, 3000)
+    })
+
+    event.waitUntil( onInstallationEnd );
 })
 
 // Cuando el SW toma el control
 
 self.addEventListener('activate', event => {
     /**
+     * STEP: 2
      * Normalmente lo que se hace en este listener es:
      * - remover cache de la version anterior
      */
