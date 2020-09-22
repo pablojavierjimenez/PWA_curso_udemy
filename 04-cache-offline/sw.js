c495223bf27f5af5955d4331cb0e4039e0346e13
Pablo Jimenez <pablojavierjimenez@gmail.com>
2020-09-22 12:04:30 -0300

    CACHEyOFFLINE: 1- detectar cuando se esta offline

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,3 +1,21 @@
 
 
 
+self.addEventListener('fetch', event => {
+
+
+    const offlineResponse = new Response(`
+        Bienvenido  a mi sitio web 
+        disculpa las molestias
+    `);
+
+    const res = fetch( event.request )
+                    .catch( () => {
+                        return offlineResponse
+                    })
+
+
+
+    event.respondWith( res );
+    console.log(event);
+})
