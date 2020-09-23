50b61466e0eab468d8b262e48c46b24dc32d486b
Pablo Jimenez <pablojavierjimenez@gmail.com>
2020-09-22 22:07:47 -0300

    CACHEyOFFLINE: 4- creando nuestro AppShell en el evento install de el service worker

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,53 +1,23 @@
 
 
+/*
+ * APP SHELL: es todo aquello que si o si necesita nuestra app
+ * para funcionar, offline y puede variar mucho segun cada aplicacion
+ */
 
-self.addEventListener('fetch', event => {
+ self.addEventListener('install', ev => {
 
-    /**
-     * STEP: 1
-     * detectar cuando esta offline y enviaruna respuesta
-     */
-    // const offlineResponse = new Response(`
-    //     Bienvenido  a mi sitio web 
-    //     disculpa las molestias
-    // `);
+    const appShell = caches.open('appshell_v1')
+        .then( cache => {
 
-    /**
-     * STEP: 2
-     * respondeos  con un un html personalizado
-     * el siguiente codigo no funciona porque devuelve un error de js
-     * o el clasico Object[object]
-     * no se puede utilizar fetch para requerir un archivo .html de este modo
-     * entonces en el siguiente paso se vera como hacer
-     *  const offlineTemplate = fetch('./pages/offline.html');
-     */
+            return cache.addAll([
+                '/index.html',
+                '/js/app.js',
+                '/css/style.css',
+                'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'
+            ]);
 
-    const offlineResponse = new Response(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-       <h1> Lo sentimos pero no tienes conneccion 😱</h1> 
-    </body>
-    </html>
-    `,
-    {
-        headers:{
-            'Content-Type':'text/html'
-        }
-    }
-    );
+        });
 
-
-
-    const res = fetch( event.request )
-                    .catch( () => offlineResponse )
-
-
-
-    event.respondWith( res );
-})
+    ev.waitUntil( appShell )
+ })
