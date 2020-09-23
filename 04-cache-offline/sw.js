6273a36d70feea45f06c16f33c93a076633c4006
Pablo Jimenez <pablojavierjimenez@gmail.com>
2020-09-23 11:21:47 -0300

    CACHEyOFFLINE: 5- Estrategias de Cache 2: Cache with Network fallback

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -4,7 +4,7 @@
  * APP SHELL: es todo aquello que si o si necesita nuestra app
  * para funcionar, offline y puede variar mucho segun cada aplicacion
  */
-
+const CACHE_NAME = 'appshell_v1';
  self.addEventListener('install', ev => {
 
     const appShell = caches.open('appshell_v1')
@@ -23,14 +23,43 @@
     ev.waitUntil( appShell )
  });
 
+/*
+ * estrategias de cache
+ */
+self.addEventListener('fetch', ev => {
 
- /*
-  * estrategias de cache
-  */
- self.addEventListener('fetch', ev => {
+   /**
+    * ESTRATEGIA 1: Cache Only
+    */
+   //ev.respondWith( caches.match( ev.request ) );
+   
+   /**
+    * ESTRATEGIA 1: Cache With Network Fallback
+    * si el archivo no se encuentra en el Cache
+    * recien ahi ir a buscarlo a la internet
+    */
+   const requestedFile =  caches.match( ev.request )
+       .then( res => {
+           if ( res ) return res;
 
-    /**
-     * ESTRATEGIA 1: Cache Only
-     */
-    ev.respondWith( caches.match( ev.request ) )
- })
+           // pero si el archivo no existe en cache
+           // tengo que ir a buscarlo en la web
+           console.log('no existe en el cache', ev.request.url);
+
+           return fetch( ev.request )
+                   .then( newRes => {
+
+                        // con esta parte lo que hacemos es que si ese archivo
+                        // que no teniamos en cache, ademas de ir a buscarlo a internet,
+                        // lo agregamos al cache para la proxima
+                       caches.open(CACHE_NAME)
+                           .then( cache => {
+                               cache.put( ev.request, newRes );
+                           });
+                       return newRes.clone();
+                   });
+       });
+   
+   ev.respondWith(requestedFile)
+
+});
