7a83d5da93920c489986ac95756a2bfccac5bc1e
Pablo Jimenez <pablojavierjimenez@gmail.com>
2020-09-24 10:56:04 -0300

    CACHEyOFFLINE: 5- Estrategias de Cache 4: Network with cache fallback

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,90 +1,109 @@
-
-
 /*
  * APP SHELL: es todo aquello que si o si necesita nuestra app
  * para funcionar, offline y puede variar mucho segun cada aplicacion
  */
 
-
-
 const CACHE_STATIC = 'static_v2';
 const CACHE_DYNAMIC = 'dynamic_v1';
 const CACHE_INMUTABLE = 'inmutable_v1';
+const CACHE_DYNAMIC_LIMIT = 10;
 
-function cleanCache( cacheName, totalItems) {
+self.addEventListener('install', (ev) => {
+    const appShell = caches.open(CACHE_STATIC).then((cache) => {
+        return cache.addAll(['/', '/index.html', '/js/app.js', '/css/style.css']);
+    });
 
-    caches.open(cacheName)
-        .then( cache => {
-            return cache.keys()
-                .then( keys => {
-                    if ( keys.length > totalItems ) {
-                        
-                        cache.delete( keys[0] )
-                            .then(cleanCache(cacheName, totalItems))
-                    }
-                })
-        })
-}
-
-self.addEventListener('install', ev => {
-
-    const appShell = caches.open(CACHE_STATIC).then( cache => {
-
-        return cache.addAll([
-            '/',
-            '/index.html',
-            '/js/app.js',
-            '/css/style.css'
+    const cacheDynamic = caches.open(CACHE_INMUTABLE).then((cache) => {
+        cache.addAll([
+            'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
         ]);
     });
-    
-    const cacheDynamic = caches.open( CACHE_INMUTABLE ).then( cache => {
-        cache.addAll(['https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css']) 
-    });
 
-
-    ev.waitUntil( Promise.all([appShell, cacheDynamic]) );
+    ev.waitUntil(Promise.all([appShell, cacheDynamic]));
 });
 
 /*
  * estrategias de cache
  */
-self.addEventListener('fetch', ev => {
+self.addEventListener('fetch', (ev) => {
+    if (!ev.request.url.includes('chrome-extension://')) {
+        /**
+         * ESTRATEGIA 1: Cache Only
+         */
+        // const response = cacheOnly(ev);
 
-   /**
-    * ESTRATEGIA 1: Cache Only
-    */
-   //ev.respondWith( caches.match( ev.request ) );
-   
-   /**
-    * ESTRATEGIA 1: Cache With Network Fallback
-    * si el archivo no se encuentra en el Cache
-    * recien ahi ir a buscarlo a la internet
-    */
+        /**
+         * ESTRATEGIA 2: Cache With Network Fallback
+         * si el archivo no se encuentra en el Cache
+         * recien ahi ir a buscarlo a la internet
+         */
+        // const response = cacheWithNetworkFallback(ev);
 
-   const requestedFile =  caches.match( ev.request )
-       .then( res => {
-           if ( res ) return res;
+        /**
+         * ESTRATEGIA 3: Network Fallback with Cache Fallback
+         */
+        const response = networkWithCacheFallback(ev);
 
-           // pero si el archivo no existe en cache
-           // tengo que ir a buscarlo en la web
-           //console.log('no existe en el cache', ev.request.url);
-
-           return fetch( ev.request )
-                   .then( newRes => {
-
-                        // con esta parte lo que hacemos es que si ese archivo
-                        // que no teniamos en cache, ademas de ir a buscarlo a internet,
-                        // lo agregamos al cache para la proxima
-                       caches.open( CACHE_DYNAMIC )
-                           .then( cache => {
-                               cache.put( ev.request, newRes );
-                               cleanCache( CACHE_DYNAMIC, 4);
-                           });
-                       return newRes.clone();
-                   });
-       });
-   
-   ev.respondWith(requestedFile)
-
+        ev.respondWith(response);
+    }
 });
+
+function cleanCache(cacheName, totalItems) {
+    caches.open(cacheName).then((cache) => {
+        return cache.keys().then((keys) => {
+            if (keys.length > totalItems) {
+                cache.delete(keys[0]).then(cleanCache(cacheName, totalItems));
+            }
+        });
+    });
+}
+
+
+function cacheOnly(ev) {
+    return ev.respondWith(caches.match(ev.request));
+}
+
+function cacheWithNetworkFallback(ev) {
+    const response = caches.match(ev.request).then((res) => {
+        if (res) return res;
+
+        // pero si el archivo no existe en cache
+        // tengo que ir a buscarlo en la web
+        //console.log('no existe en el cache', ev.request.url);
+
+        return fetch(ev.request).then((newRes) => {
+            // con esta parte lo que hacemos es que si ese archivo
+            // que no teniamos en cache, ademas de ir a buscarlo a internet,
+            // lo agregamos al cache para la proxima
+            caches.open(CACHE_DYNAMIC).then((cache) => {
+                cache.put(ev.request, newRes);
+                cleanCache(CACHE_DYNAMIC, 4);
+            });
+            return newRes.clone();
+        });
+    });
+
+    return response;
+}
+
+function networkWithCacheFallback(ev) {
+    const response = fetch(ev.request)
+        .then((res) => {
+            // esta linea de abajo se agrega porque un 404 no es atrapado por el catch
+            // asi que hay que hacer un fallback a eso tambien
+            if (!res) return caches.match(ev.request);
+
+            caches.open(CACHE_DYNAMIC).then((cache) => {
+                cache.put(ev.request, res);
+                cleanCache(CACHE_DYNAMIC, CACHE_DYNAMIC_LIMIT);
+            });
+
+            return res.clone();
+        })
+        .catch((err) => {
+            return caches.match(ev.request);
+        });
+
+    return response;
+}
+
